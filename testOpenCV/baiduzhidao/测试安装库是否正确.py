76dffa2092f9a279d2e6cb766286a52b8f93ac5d
lgp <liguangpei1@163.com>
2019-07-16 00:36:56 +0800

    深入浅出机器学习第二章练习代码上传

---
+++ testOpenCV/baiduzhidao/测试安装库是否正确.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+import pandas
+print(pandas.__version__)
+
+import numpy
+print(numpy.__version__)
+
+import matplotlib
+print(matplotlib.__version__)
+
+import ipython_genutils
+print(ipython_genutils.__version__)
